5fc4121cef803c3720b5f3abaec23813e04fa619
Eduardo Alencar <ealencar10@gmail.com>
2024-08-19 11:26:08 -0300

    test: implementa teste para rota vowel_count

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -10,3 +10,15 @@ def test_hello_world() -> None:
     assert response.status_code == HTTPStatus.OK
     content = response.json()
     assert content['message'] == "Hello World"
+
+def test_vowel_count() -> None:
+    client = TestClient(app)
+    data = {"words": ["batman", "robin", "coringa"]}
+    response = client.post(
+        "/vowel_count",
+        json=data
+    )
+    content = response.json()
+    assert content == {"batman": 2, "robin": 2, "coringa": 3}
+
+    
