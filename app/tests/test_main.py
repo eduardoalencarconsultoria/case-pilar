1cc94d70014366d44366419cbe441c9b1b6e1998
Eduardo Alencar <ealencar10@gmail.com>
2024-08-19 12:23:34 -0300

    tests: cria cenários de testes para rota sort

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -100,3 +100,41 @@ def test_sort_with_invalid_order() -> None:
     )
     content = response.json()
     assert content['detail'][0]['msg'] == "Input should be 'asc' or 'desc'"
+
+def test_sort_bad_requst() -> None:
+    client = TestClient(app)
+    data = {"words": 123, "order": "invalid"}
+    response = client.post(
+        "/sort",
+        json=data
+    )
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY 
+    content = response.json()
+    assert content['detail'][0]['msg'] == "Input should be a valid list"
+
+def test_sort_with_invalid_content() -> None:
+    client = TestClient(app)
+    data = {"words": ["batman", "robin", "coringa"], "order": "invalid"}
+    response = client.post(
+        "/sort",
+        headers={'Content-Type': 'application/pdf'},
+        json=data
+    )
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY 
+
+def test_sort_with_invalid_http_method() -> None:
+    client = TestClient(app)
+    data = {"words": ["batman", "robin", "coringa"], "order": "invalid"}
+    response = client.get(
+        "/sort",
+    )
+    assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
+
+def test_sort_with_invalid_request_body() -> None:
+    client = TestClient(app)
+    data = {"words": [1,2,3], "order": "invalid"}
+    response = client.post(
+        "/sort",
+        json=data
+    )
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY 
