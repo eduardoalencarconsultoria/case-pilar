31fe29c7841de88d5f992d4db141dfa47ee50735
Eduardo Alencar <ealencar10@gmail.com>
2024-08-19 11:26:48 -0300

    feat: implementa lógica para rota vowel_count

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +7,24 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+class VowelCountRequest(BaseModel):
+    words: list
+
+@app.post("/vowel_count")
+async def vowel_count(vowel_count_request: VowelCountRequest):
+    print(vowel_count_request.words)
+    response = {}
+    for word in vowel_count_request.words:
+        response.update({word: _vowel_count(word)})
+    return response
+    
+def _vowel_count(word: str) -> int:
+    count = 0
+    vowel = set("aeiouAEIOU")
+
+    for letter in word:
+        if letter in vowel:
+            count = count + 1
+
+    return count
