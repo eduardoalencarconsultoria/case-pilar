7209ec16ef245935e5b407112d371c24e4c6593b
Eduardo Alencar <ealencar10@gmail.com>
2024-08-19 12:15:33 -0300

    feat: cria enum para opções de ordem na rota sort

--- app/model.py
+++ app/model.py
@@ -1,7 +1,12 @@
+from enum import Enum
 from pydantic import BaseModel
 
 class VowelCountRequest(BaseModel):
     words: list[str]
 
+class OrderEnum(str, Enum):
+    asc = 'asc'
+    desc = 'desc'
+
 class SortRequest(VowelCountRequest):
-    order: str
+    order: OrderEnum
